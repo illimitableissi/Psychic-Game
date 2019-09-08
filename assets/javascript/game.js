24b953e338cb50117a980d0858b7701a4c0e6419
SOVEREIGN <isimemeni@gmail.com>
2019-09-08 14:48:04 -0400

    added audio elements

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,13 @@
 
 // Game Logics
-var currentLtrIndex = "";
-var answerDisplay = [];
+var wrongLetter = [];
 
 // Stats
 var wins = 0;
 var losses = 0;
-var remainingGuesses = 5;
+var remainingGuesses = 8;
 
 // Arrays listing every letter of the alphabet
-var userchoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" ]
 
 // Variables that hold references to the places in the HTML.
@@ -17,57 +15,54 @@ var displayWins = document.getElementById("display-wins");
 var displayLosses = document.getElementById("display-losses");
 var guessesRemaining = document.getElementById("guesses-remaining");
 var alreadyGuessed = document.getElementById("already-guessed");
-var audioElement = document.createElement("audio");
-    audioElement.setAttribute("src", "./assets/sounds/Get Over Here!.mp3");
+var scorpion = document.getElementById("Scorpion");
+var fatality = document.getElementById("Fatality")
 
 
 // New Game
-function newGame() {
+function newGame () {
+// Generates random letter from  array
+    computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        console.log("The computer's choice is: " + computerGuess);
 
-    // Generates random letter from  array
-    currentLtrIndex = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-        console.log("The current letter is: " + currentLtrIndex);
-}   
-    //resets variables
-    remainingGuesses = 5;
-    answerDisplay = [];
-    wins = 0;
-    losses = 0;
 
-    //reset HTML variables
-    guessesRemaining.textContent = "Number of Guesses Remaining: " + " " + remainingGuesses;
-    displayWins.textContent = "Wins: " + " " + wins;
+// Updates fields
+    remainingGuesses = 8;
+    wrongLetter = [];
+    displayWins.textContent ="Wins: " + " " + wins;
     displayLosses.textContent = "Losses: " + " " + losses;
+    guessesRemaining.textContent ="Number of Guesses Remaining: " + " " + remainingGuesses;
+    alreadyGuessed.textContent ="Letters Already Guessed" + " " + wrongLetter;
+}
+
+ document.onkeydown = function(event) {
+    var userGuess = event.key
+        console.log(userGuess)
         
+//if user guesses correctly
+    if(userGuess === computerGuess) {
+        scorpion.play();
+        alert("You guessed " + computerGuess + " correctly!");
+        displayWins.textContent = "Wins: " + " " + wins++;
+        newGame();
+    
+//when player guess is incorrect 
+    } else  {
+        remainingGuesses-- ;
+        displayLosses.textContent = "Losses: " + " " + losses;
+        alreadyGuessed.textContent = "Letters Already Guessed: " + " " + wrongLetter.push(userGuess);
+    }
 
-// // Round by Round
-// function round(){
+    if(remainingGuesses === -0) {
+        fatality.play();
+        alert("FATALITY!!")
+        losses++;
+        newGame();
+        
+    }
+}
+newGame()
 
-//     currentWord.textContent = "Current Word: " + " " + answerDisplay.join(" ");
-//     guessesRemaining.textContent = "Number of Guesses Remaining: " + " " + remainingGuesses;
-//     alreadyGuessed.textContent = "Letters Already Guessed: " + " " + wrongLetters.join(" ")
 
 
-// //When player wins
-//     if (currentWrdLetters.toString() == answerDisplay.toString()) {
-//         wins++;
-//         alert("You guessed ;" + currentWordindex + " ")
-//         audioElement.play()
-//         displayWins.textContent = "Wins: " + " " + wins;
-
-//         newGame();
-//         alreadyGuessed.textContent = "Letters Already Guessed: " + " " + " ";
-// //when player losses
-//     } else if (remainingGuesses == -1) {
-//         losses++;
-//         alert("FATALITY")
-//         displayLosses.textContent = "Losses: " + " " + losses;
-//         newGame();
-//         alreadyGuessed.textContent = "Letters Already Guessed: " + " " + " ";
-//     }
-// }
-
-// // Play the Game
-newGame();
-// round();
 
